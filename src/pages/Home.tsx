7e680352c33eb04f433a32560c156740b193a803
Rishabh Singh <ris8645208@gmail.com>
2021-08-15 14:00:07 +0530

    styles added rubik font added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import FetchWeatherData from "../Api/FetchWeatherData";
 import isWeather from "../Interface/interface";
-// import "./App.css";
+import "./home.css";
 import dummyData from "../data/data";
 
 export const Home = () => {
@@ -19,36 +19,45 @@ export const Home = () => {
     }
   };
   return (
-    <div className="main-container">
-      <h2 className="head">Get weather details on search</h2>
-      <input
-        type="text"
-        className="search"
-        placeholder="Search any place..."
-        value={query}
-        onChange={(event) => setQuery(event.target.value)}
-        onKeyPress={search}
-      />
-      {weather.main && (
-        <div className="city">
-          <h2 className="city-name">
-            <span>{weather.name}</span>
-            <sup>{weather.sys.country}</sup>
-          </h2>
-          <div className="city-temperature">
-            {Math.round(weather.main.temp)}
-            <sup>&deg;C</sup>
-            <div className="info">
-              <img
-                className="city-icon"
-                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-                alt={weather.weather[0].description}
-              />
-              <p>{weather.weather[0].description}</p>
-            </div>
+    <div className="Home">
+      <div className="section-header">
+        <h2 className="head">Get weather details on search</h2>
+        <p className="para">Search by city name !</p>
+      </div>
+      <div className="weather-container">
+        <div className="wrapper">
+          <div className="weather-card">
+            <input
+              type="text"
+              className="search"
+              placeholder="Enter a place ...."
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
+              onKeyPress={search}
+            />
+            {weather.main && (
+              <div className="city">
+                <h2 className="city-name">
+                  <span>{weather.name}</span>
+                  <sup>{weather.sys.country}</sup>
+                </h2>
+                <div className="city-temperature">
+                  {Math.round(weather.main.temp)}
+                  <sup>&deg;C</sup>
+                  <div className="info">
+                    <img
+                      className="city-icon"
+                      src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                      alt={weather.weather[0].description}
+                    />
+                    <p>{weather.weather[0].description}</p>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
